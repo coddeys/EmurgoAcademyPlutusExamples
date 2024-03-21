f5a008473ef6718b6628dfcad9c6b3fe55213fd9
coddeys <dima@sukhikh.xyz>
2024-03-21 18:40:57 +0100

    Burn wip

--- V2_110/DemeterAndDocker/3_NativeTokens/testnet/burn.sh
+++ V2_110/DemeterAndDocker/3_NativeTokens/testnet/burn.sh
@@ -1,11 +1,9 @@
-utxoin1="721ede6f5113f91e1a90b919735667e202d6738d937f0857b00dbcef0287424a#1"
-policyid=$(cat ieaNFT.pid)
-nami="addr_test1qpc6mrwu9cucrq4w6y69qchflvypq76a47ylvjvm2wph4szeq579yu2z8s4m4tn0a9g4gfce50p25afc24knsf6pj96sz35wnt"
-output="11000000"
-tokenamount="-1"
-tokenname=$(echo -n "cantburnEAnft" | xxd -ps | tr -d '\n')
-collateral="2b55046a8742d6e149e0c19cd920c691574133341bb695a952a946c45a000d0b#2"
-signerPKH="697a501b7d05766b3d08e39dab43e0f170973d3398b28745b3b8ce55"
+utxoin1="c7e5859f6c3920e54bc81e6638980a749c5c897fb428cfe5611c99a57542cd7b#1"
+policyid=$(cat EAcoins.pid)
+tokenamount="-100"
+tokenname=$(echo -n "EAcoinsbat107" | xxd -ps | tr -d '\n')
+collateral="c7e5859f6c3920e54bc81e6638980a749c5c897fb428cfe5611c99a57542cd7b#1"
+signerPKH="8b225ceddb05738d7a53bd130136e187a6f0baa4d219161fed4f2ac0"
 notneeded="--invalid-hereafter 10962786"
 PREVIEW="--testnet-magic 2"
 
@@ -13,24 +11,21 @@ cardano-cli transaction build \
   --babbage-era \
   $PREVIEW \
   --tx-in $utxoin1 \
-  --tx-in "f5b3d2db69918ac9ecb3f2d284eb3e325c58ef02bafe2438a1f59f301a920f28#0"  \
+  --tx-in "c9899a4a611bc7d105829dda4a2473a5f6733568d67bc265e7bfde30814547bf#0"  \
   --required-signer-hash $signerPKH \
   --tx-in-collateral $collateral \
-  --tx-out $nami+$output \
   --change-address $Adr01 \
   --mint "$tokenamount $policyid.$tokenname" \
-  --mint-script-file ieaNFT.plutus \
-  --mint-redeemer-file unit.json \
-  --protocol-params-file protocol.params \
+  --mint-script-file EAcoins.plutus \
+  --mint-redeemer-file OurRedeemer.json \
   --out-file burnTx.body
 
 cardano-cli transaction sign \
     --tx-body-file burnTx.body \
-    --signing-key-file ../../../../../Wallets/Adr07.skey \
-    --signing-key-file ../../../../../Wallets/Adr01.skey \
+    --signing-key-file ../../../wallets/person1.skey \
     $PREVIEW \
     --out-file burnTx.signed
 
- cardano-cli transaction submit \
+cardano-cli transaction submit \
     $PREVIEW \
     --tx-file burnTx.signed
